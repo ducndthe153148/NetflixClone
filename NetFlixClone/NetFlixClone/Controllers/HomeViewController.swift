ea56785d4ff8e4838fefa2431b3ac8aecabd1e88
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-06-23 01:11:01 +0700

    init code

---
+++ NetFlixClone/NetFlixClone/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  NetFlixClone
+//
+//  Created by MacOS on 23/06/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
---
+++ NetFlixClone/NetFlixClone/MainTabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NetFlixClone
+//
+//  Created by MacOS on 23/06/2022.
+//
+
+import UIKit
+
+class MainTabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+}
+
