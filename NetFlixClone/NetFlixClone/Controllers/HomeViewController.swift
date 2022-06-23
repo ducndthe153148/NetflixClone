0112dc02642270915d593993042b49bd16d97ff0
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-06-23 17:17:31 +0700

    cell

--- NetFlixClone/NetFlixClone/Controllers/HomeViewController.swift
+++ NetFlixClone/NetFlixClone/Controllers/HomeViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        table.registerCell(for: "MovieTableViewCell")
+        table.delegate = self
+        table.dataSource = self
     }
 
+}
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell", for: indexPath) as! MovieTableViewCell
+        cell.textLabel?.text = "Hello world";
+        return cell;
+    }
+    
+}
+
+extension UITableView {
+    func registerCell(for id: String) {
+        let nib = UINib(nibName: id, bundle: nil)
+        register(nib, forCellReuseIdentifier: id)
+    }
 }
---
+++ NetFlixClone/NetFlixClone/Presentation/Home/MovieCell/MovieCell.swift
@@ -0,0 +1,8 @@
+//
+//  MovieCell.swift
+//  NetFlixClone
+//
+//  Created by MacOS on 23/06/2022.
+//
+
+import Foundation
